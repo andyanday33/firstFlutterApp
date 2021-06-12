5b525704fc1a6a0463c6950210a4bfbfa60ddd88
andyanday33 <andyanday33@gmail.com>
2021-06-12 13:00:13 +0300

    init

---
+++ lib/quote.dart
@@ -0,0 +1,11 @@
+class Quote {
+
+  String text = '';
+  String author = '';
+
+  Quote({required this.text,required this.author } ) {
+  }
+
+}
+
+Quote myquote = Quote(text : "This is the qite text", author: "oscar wilde");
